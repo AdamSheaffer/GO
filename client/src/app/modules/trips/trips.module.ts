348aa9199169553a9a5d3284229c9abca69f5b40
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2018-03-07 21:41:51 -0600

    spread components array in declarations

--- client/src/app/modules/trips/trips.module.ts
+++ client/src/app/modules/trips/trips.module.ts
@@ -15,7 +15,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
   ],
   declarations: [
-    COMPONENTS
+    ...COMPONENTS
   ]
 })
 export class TripsModule { }
